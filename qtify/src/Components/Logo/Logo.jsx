45aa95414737fcd3dee02601f7415803c1752928
Samarth Badkur <samarthbadkur11@gmail.com>
2025-05-21 18:10:47 +0530

    Changes

--- qtify/src/Components/Logo/Logo.jsx
+++ qtify/src/Components/Logo/Logo.jsx
@@ -2,5 +2,5 @@ import React from "react";
 import LogoImage from "../../assets/logo.png";
 
 export default function Logo() {
-  return <img src={LogoImage} alt="logo" width={67} />;
+  return <img src={LogoImage} alt="logo" width={67} style={{ verticalAlign: "middle" }}/>;
 }
